be9aa58f32817705f125d04b4d67d1628a4bd3c2
Arash Manteghi <arash.manteghi@gmail.com>
2018-01-20 20:11:26 +0330

    Add basic show/hide animation effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
+import Modalian from './Modalian';
+
 // styles
 import './App.scss';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      modalVisible: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal () {
+    this.setState({ modalVisible: !this.state.modalVisible });
+  }
+
   render () {
     return (
       <div>
-        Hello!!
+        <button onClick={() => { this.setState({ modalVisible: !this.state.modalVisible }); }}>open</button>
+        <Modalian
+          visible={this.state.modalVisible}
+          onClose={this.toggleModal}
+        >
+          <h2 className='modalian__header'>Test Modalian</h2>
+        </Modalian>
       </div>
     );
   }
---
+++ src/Modalian.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import closeIcon from './close.svg';
+
+import './Modalian.scss';
+
+export default class Modalian extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      modalianClass: 'modalian modalian--none'
+    };
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.visible !== nextProps.visible) {
+      nextProps.visible
+        ? this.setState({modalianClass: 'modalian modalian--show'})
+        : this.setState({modalianClass: 'modalian modalian--hide'});
+      setTimeout(() => {
+        nextProps.visible
+          ? this.setState({modalianClass: 'modalian modalian--show'})
+          : this.setState({modalianClass: 'modalian modalian--none'});
+      }, 410);
+    }
+  }
+
+  render () {
+    console.log('rerender: ', this.state.modalianClass);
+    return (
+      <div className={this.state.modalianClass}>
+        <div className='modalian-mask' onClick={this.props.onClose} />
+        <div className='modalian-wrapper'>
+          {this.props.children}
+        </div>
+        {/* <img src={closeIcon} onClick={() => { console.log('click'); }} />
+        {this.props.children} */}
+      </div>
+    );
+  }
+}
+
+Modalian.propTypes = {
+  children: PropTypes.node,
+  visible: PropTypes.bool
+};
